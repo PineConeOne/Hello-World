5d78033099869ac4a059251fc1bcdc663b504a49
nova_ <nova_jansson@hotmail.com>
2026-03-30 10:42:57 +0200

    Hello Pluto.

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -3,6 +3,6 @@ package helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Universe!");
-        System.out.println("Hallå Jupiter!");
+        System.out.println("Hallå Pluto!");
     }
 }
