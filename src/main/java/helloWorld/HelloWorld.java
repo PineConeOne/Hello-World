26b6af45a1d48412bdca20d9d6f9b32e3a303788
staff <staffan.aaberg@gmail.com>
2026-03-30 11:41:35 +0200

    Hej hej!

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Universe!");
         System.out.println("Hallå Pluto!");
+        System.out.println("Hej hej!!");
     }
 }
