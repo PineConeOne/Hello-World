594c821304e0e6c1e771a9c928b10cc4596a47ec
staff <staffan.aaberg@gmail.com>
2026-04-01 13:16:57 +0200

    Hejsan svejsan

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -2,7 +2,7 @@ package helloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Universe!");
+        System.out.println("Hejsan svejsan Universitetet!");
         System.out.println("Hallå Pluto!");
     }
 }
